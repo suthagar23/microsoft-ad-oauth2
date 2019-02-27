635698e6fbc33c5eaf728cf7cab732b15660b8d2
Tharmarajasingam Thuvarakan <tharma.thuva@gmail.com>
2019-02-27 12:09:25 +0530

    1. Clean the token in the logout function 2. Check the token validity in the component mount method
    Signed-off-by: Tharmarajasingam Thuvarakan <tharma.thuva@gmail.com>

--- App.js
+++ App.js
@@ -3,8 +3,22 @@ import { Linking, WebBrowser } from 'expo';
 import { StyleSheet, Text, View, Button, AsyncStorage } from 'react-native';
 import {ReactNativeAD, ADLoginView, Logger} from './azureAD/index.js'
 
-const CLIENT_ID = 'b4739248-e8fb-4d15-8f47-c0eb173fec81'
-const KEY = 'b4739248-e8fb-4d15-8f47-c0eb173fec81.https://graph.microsoft.com'
+
+const CONFIG = {
+  client_id : 'b4739248-e8fb-4d15-8f47-c0eb173fec81',
+  // redirectUrl : 'http://localhost:8080(optional)',
+  // authorityHost : 'https://login.microsoftonline.com//oauth2/authorize',
+  // tenant  : 'common(optional)',
+  // client_secret : 'client-secret-of-your-app(optional)',
+  resources : [
+    'https://graph.microsoft.com',
+    // 'https://outlook.office.com',
+    // 'https://outlook.office365.com',
+    // 'https://wiadvancetechnology.sharepoint.com',
+    // 'https://graph.windows.net',
+  ]
+}
+
 
 Logger.setLevel('VERBOSE')
 export default class App extends React.Component {
@@ -14,25 +28,38 @@ export default class App extends React.Component {
       azureAdOauthActive: false,
       azureLoginPortalVisible: false,
       azureLogoutPortalVisible: false,
-      // logout: false,
-      // logoutButtonVisible: false,
-      // loginShow: false,
+      reactNativeAD: new ReactNativeAD(CONFIG)
     }
   }
 
-  componentDidMount(){
-    let lastLoginValue = AsyncStorage.getItem(KEY)
-    lastLoginValue.then((value) => {
-      console.log("lastLoginValue : ", value)
-      if (value !== null) {
-        this.setState(Object.assign({}, this.state, { azureAdOauthActive: true, azureLogoutPortalVisible: false}))
+  async componentDidMount(){
+    for (let resource of CONFIG.resources){
+      let credential = await this.state.reactNativeAD.checkCredential(resource)
+      if(credential===null){
+        await this.cleanTokens(CONFIG.resources);
+        return;
+      } else {
+        let expires_on = credential.expires_on*1000
+        if(Date.now() - expires_on > -60000) {
+          await this.cleanTokens(CONFIG.resources)
+          return;
+        }
       }
-    })
+    }
+    this.setState(Object.assign({}, this.state, { azureAdOauthActive: true, azureLogoutPortalVisible: false}))
   }
 
   onLoginSuccess(credentials) {
     console.log(credentials)
     this.setState(Object.assign({}, this.state, { azureAdOauthActive: true, azureLoginPortalVisible: false}))
+  }
+
+  async cleanTokens(resources) {
+    console.log("Clean Tokens")
+    for (let resource in resources){
+      let key = `${CONFIG.client_id}.${resource}`
+      await AsyncStorage.removeItem(key)
+    }
   }
 
   afterLogout(status) {
@@ -44,19 +71,13 @@ export default class App extends React.Component {
   }
 
   async onLogout() {
-    console.log("Logout")
-    await AsyncStorage.removeItem(KEY)
-    this.setState(Object.assign({}, this.state, { azureAdOauthActive: false, azureLoginPortalVisible:true, azureLogoutPortalVisible: true}))
+    console.log("Logout");
+    await this.cleanTokens(CONFIG.resources);
+    this.setState(Object.assign({}, this.state, { azureAdOauthActive: false, azureLoginPortalVisible:true, azureLogoutPortalVisible: true}));
 
   }
 
-  render() {
-    new ReactNativeAD({
-      client_id: CLIENT_ID,
-      resources: [
-        'https://graph.microsoft.com'
-      ]})
-    
+  render() {    
     return (
       <View style={styles.container}>
         {this.state.azureAdOauthActive ? 
@@ -69,7 +90,7 @@ export default class App extends React.Component {
           <View>
             {this.state.azureLoginPortalVisible ? 
               <ADLoginView
-              context={ReactNativeAD.getContext(CLIENT_ID)}
+              context={ReactNativeAD.getContext(CONFIG.client_id)}
               onSuccess={this.onLoginSuccess.bind(this)}
               afterLogout={this.afterLogout.bind(this)}
               hideAfterLogin={true}
